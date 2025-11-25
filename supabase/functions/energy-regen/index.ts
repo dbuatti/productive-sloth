66bae71ba90586101974e42d5ce1f9593a9b61d2
Daniele Buatti <info@danielebuatti.com>
2025-11-25 13:35:21 +1100

    [dyad] Fixed missing date-fns import in the energy-regen Edge Function. - wrote 1 file(s)

--- supabase/functions/energy-regen/index.ts
+++ supabase/functions/energy-regen/index.ts
@@ -3,7 +3,7 @@ import { serve } from "https://deno.land/std@0.190.0/http/server.ts";
 // @ts-ignore
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.45.0';
 // @ts-ignore
-import { differenceInMinutes, parseISO, setHours, setMinutes, startOfDay, addDays, isBefore, isAfter, min } from 'https://esm.sh/date-fns@2.30.0';
+import { differenceInMinutes, parseISO, setHours, setMinutes, startOfDay, addDays, isBefore, isAfter, min, addMinutes } from 'https://esm.sh/date-fns@2.30.0';
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
